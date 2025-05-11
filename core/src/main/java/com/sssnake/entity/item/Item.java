e76ea3ca80685b6a7ddd4351da50c91470c64cf9
31n5ang <31n5ang@gmail.com>
2025-05-11 18:04:25 +0900

    refactor: remove unnecessary logic

--- core/src/main/java/com/sssnake/entity/item/Item.java
+++ core/src/main/java/com/sssnake/entity/item/Item.java
@@ -8,8 +8,6 @@ public abstract class Item extends Coordination implements Displayable {
 
     private final SpawnStrategy spawnStrategy;
     protected final DisplaySymbol displaySymbol;
-    protected boolean isDisplayed = true;
-    protected boolean isCanBeConsumed = true;
 
     public Item(SpawnStrategy spawnStrategy, DisplaySymbol displaySymbol) {
         super(0, 0);
@@ -26,21 +24,5 @@ public abstract class Item extends Coordination implements Displayable {
         Coordination spawnCoord = spawnStrategy.spawn();
         setX(spawnCoord.getX());
         setY(spawnCoord.getY());
-    }
-
-    public boolean isDisplayed() {
-        return isDisplayed;
-    }
-
-    public void setDisplayed(boolean isDisplayed) {
-        this.isDisplayed = isDisplayed;
-    }
-
-    public boolean isCanBeConsumed() {
-        return isCanBeConsumed;
-    }
-
-    public void setCanBeConsumed(boolean canBeConsumed) {
-        isCanBeConsumed = canBeConsumed;
     }
 }
