f53c490f74fa3c9bab8d0791b603e632945349ac
31n5ang <31n5ang@gmail.com>
2025-05-11 17:59:45 +0900

    refactor: extract constants into config

---
+++ core/src/main/java/com/sssnake/system/GameConfig.java
@@ -0,0 +1,7 @@
+package com.sssnake.system;
+
+public class GameConfig {
+    public static final int MAP_WIDTH = 32;
+    public static final int MAP_HEIGHT = 16;
+    public static final int REDISPLAY_DELAY = 100; // ms
+}
